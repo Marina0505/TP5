a1b6830b1aca808ddefcd4657aa1b7b3f79077d7
Marina <45629529+Marina0505@users.noreply.github.com>
2018-12-12 14:37:14 +0100

    TP Github / Captcha / Session

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
+<!doctype html> 
+<html>
+  <head> 
+  <meta charset="utf-8">
+  </head> 
+	<body>
 <?php
 
 /* SCRIPT FRONTAL AFFICHAGE DE CAPTCHA
-* AFFICHER UNE IMAGE GÉNÉRÉE EN PHP */
+* AFFICHER UNE IMAGE GÃ‰NÃ‰RÃ‰E EN PHP */
 
 echo "<img src='script-captchas.php' alt='captchas' />";
 
 ?>
+  </body>
+</html>
